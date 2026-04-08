2bcc4cea39125ce7432182ea9d214a1941336735
João Pedro Gouvea <joaopedrosouza1324@gmail.com>
2026-04-08 15:36:21 -0300

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Administrador',
             'email' => 'admin@example.com',
             'password' => bcrypt('senha123'),
         ]);
+       
+
     }
 }
